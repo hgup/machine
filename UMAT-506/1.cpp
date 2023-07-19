a043347643bc3ad4f0852a137402376a542497b5
Hursh Gupta <hurshgupta1@gmail.com>
2023-07-19 07:55:44 +0000

    init 5th sem

---
+++ UMAT-506/1.cpp
@@ -0,0 +1,11 @@
+/*
+ * Prints a triangle using asterisks
+ * AUTHOR: Hursh Gupta
+ * WRITTEN ON: 7-17-M
+ */
+
+#include <stdio.h>
+
+int main() {
+	return 0;
+}
---
+++ UMAT-506/2.cpp
@@ -0,0 +1,13 @@
+
+/*
+ * Read and write to variables
+ * AUTHOR: Hursh Gupta
+ * WRITTEN ON: 7-17-M
+ */
+
+#include <stdio.h>
+
+int main() {
+	// scanf
+	// printf
+}
