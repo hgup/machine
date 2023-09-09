b501785790cc573a4a2be767cefad6686bdee1dd
hgup <hurshgupta1@gmail.com>
2023-09-09 10:40:03 +0530

    finished assignments

--- UMAT-506/H028.c
+++ UMAT-506/H028.c
@@ -6,6 +6,50 @@
 
 #include <stdio.h>
 
+double exp(double x){
+	double sum = 1;
+	double add = 1;
+	int i = 0;
+	do{
+		i++;
+		add *= x/i;
+		sum += add;
+		//printf("%d) %lf \n",i,add); some debug code
+	} while( add > 0.001);
+	return sum;
+} // works
+
+double sin(double x){
+	double sum = x;
+	double add = 1;
+	int i = 1;
+	do{
+		i += 2;	
+		add *= -1 * x*x/((i-1)*i);
+		sum += add;
+	} while( add > 0.001);
+	return sum;
+}
+
+double cos(double x){
+	double sum = 1;
+	double add = 1;
+	int i = 0;
+	do{
+		i += 2;		
+		add *= -1 * x*x/((i-1)*i);
+		sum += add;
+	} while( add > 0.001);
+	return sum;
+}
+
 int main() {
+	double n;
+	printf("Enter number: ");
+	scanf("%lf",&n);
+	printf("exp(%4.2lf) \t= %10.4lf\n", n, exp(n));
+	printf("sin(%4.2lf) \t= %10.4lf\n", n, sin(n));
+	printf("cos(%4.2lf) \t= %10.4lf\n", n, cos(n));
+	
 	return 0;
 }
--- UMAT-506/H040.c
+++ UMAT-506/H040.c
@@ -4,8 +4,76 @@
  * WRITTEN ON: 
  */
 
+void copyArray(double[], double[], int);
+void readArray(double[], int);
+void bubbleSort(double[], int);
+void selectionSort(double[], int);
+void printArray(double[], int);
+
 #include <stdio.h>
+#define N 5
 
 int main() {
-	return 0;
+	double A[N], B[N];
+	readArray(A, N);
+	copyArray(A, B, N);
+	printf("\nBubble sort========\n");
+	bubbleSort(A, N);
+	printArray(A, N);
+	printf("\nSelection sort========\n");
+	selectionSort(B, N);
+	printArray(B, N);
+}
+
+
+void copyArray(double A[], double B[], int size){
+// copies B <- A (A remains unchanged)
+
+	for(int i = 0; i < size; i++)
+		B[i] = A[i];
+}
+
+void readArray(double A[], int size){
+
+	printf("\nENTER THE ARRAY\n================\n");
+	for(int i = 0; i < size; i++){
+		printf("Enter #%d: ", i); 
+		scanf("%lf", &A[i]); 
+	}
+
+}
+
+
+void _swap(double A[], int a, int b){
+	double temp;
+	temp = A[a];
+	A[a] = A[b];
+	A[b] = temp;
+}
+
+void bubbleSort(double A[], int size){
+	for(int i = 0; i < size; i++)
+		for(int j = i; j < size -1; j++)
+			if(A[j+1] < A[j]) _swap(A, j, j+1);
+	
+}
+
+void selectionSort(double A[], int size){
+	int mindex = 0;
+	for(int i = 0; i < size-1; i++){ 
+
+		mindex = i;
+		for(int j = i+1; j < size; j++)
+			if(A[j] < A[mindex]) mindex = j;
+
+		_swap(A, i, mindex);
+	}
+}
+
+void printArray(double A[], int size){
+	printf("\nARRAY ELEMENTS\n================\n");
+
+	for(int i = 0; i < size; i++){
+		printf("Element #%d:\t %7.2lf \n", i, A[i]);
+	}
 }
