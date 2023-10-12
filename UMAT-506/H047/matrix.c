a18199f19e7db96b4888f3b0ea274f0aca0746e8
hgup <hurshgupta1@gmail.com>
2023-10-12 12:29:33 +0530

    finished H047

--- UMAT-506/H047/matrix.c
+++ UMAT-506/H047/matrix.c
@@ -6,10 +6,12 @@
 // Allocates 2-D double array of r and c size
 double ** allocate(int r, int c)
 {
-  double **p = (double**) malloc(r * sizeof(double**));   // malloc -> returns pointer to pointers
+  double **p = (double**) malloc(r * sizeof(double**));
+  // malloc -> returns pointer to pointers
 
   for(int i = 0; i < r; i++){
-    *(p+i) = (double*) malloc(c * sizeof(double));        // malloc -> returns pointers to pointers
+    *(p+i) = (double*) malloc(c * sizeof(double));
+    // malloc -> returns pointers to pointers
   }
 
   return p;
@@ -66,3 +68,33 @@ double ** add_matrix(double ** a, double ** b, int r, int c){
        
   return sum;
 }
+
+//Computes difference of a and b and returns in new matrx
+double ** subtract_matrix(double ** a, double ** b, int r, int c){
+  double **sum = allocate(r, c);
+  for(int i = 0; i < r; i++)
+    for (int j = 0; j < r; j++)
+      sum[i][j] = a[i][j] - b[i][j];
+       
+  return sum;
+}
+
+//Computes product of a and b and returns in new matrix
+double ** product_matrix(double ** a,  int r1, int c1,double ** b, int r2, int c2){
+  double **product = allocate(r1, c2);
+  int p = c1;       // (m x p) and (p x n)
+  
+  if (c1 != r2){
+    printf("ERROR: Attempting to print incompatible matrices\n");
+    return product;   // return garbage TODO: how to throw error
+  }
+
+  for(int i = 0; i < r1; i++){
+    for(int j = 0; j < c2; j++){
+      for(int k = 0; k < p; k++) product[i][j] += a[i][p] * b[p][j];
+    }
+  }
+
+  return product;
+}
+
